90410eddcbce9eeb7e6125b66af38f63a6b0b12a
Saule Suiunalieva <talaibekovnasaule@gmail.com>
2022-05-17 12:31:51 +0600

    Initial Commit

---
+++ HomeWork1Month1Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork1Month1Storyboard
+//
+//  Created by saule on 17/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
